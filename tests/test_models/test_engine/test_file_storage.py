3a1a745a472d0a73354658f720a17bcdb2fbf6c7
Cassyeh <cassandraijezie@gmail.com>
2023-10-16 03:13:35 +0000

    Test file for filestorage engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Unittest for FileStorage class
+"""
+import unittest
+import os
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test Cases for the FileStorage class.
+    """
+    def test_attrs(self):
+        """
+        Test for attributes
+        """
+        fs = FileStorage()
+        self.assertEqual(type(fs._FileStorage__file_path), str)
+        self.assertEqual(type(fs._FileStorage__objects), dict)
+        self.assertTrue(hasattr(FileStorage, "all"))
+        self.assertTrue(hasattr(FileStorage, "save"))
+        self.assertTrue(hasattr(FileStorage, "reload"))
+        self.assertTrue(hasattr(FileStorage, "new"))
+        self.assertIsInstance(fs, FileStorage)
+        self.assertEqual(type(fs.all()), dict)
+        self.assertTrue(type(fs.new), "<class 'method'>")
+    
+    def test_save_reload(self):
+        """
+        Testing FileStorage save method
+        """
+        fs = FileStorage()
+        file_path = "file.json"
+        fs.save()
+        self.assertTrue(os.path.exists(file_path))
+        ids = []
+        for i in range(5):
+            bm = BaseModel()
+            fs.new(bm)
+            ids.append(bm.id)
+        fs.save()
+        fs.reload()
+        all_reloaded = fs.all()
+        self.assertTrue(len(all_reloaded.keys()) == len(ids))
